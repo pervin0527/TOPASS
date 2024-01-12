10d5bb81334dd0adaefaa0ad104454a41c1c8561
pervin0527 <zfbtldnz@gmail.com>
2024-01-12 23:48:13 +0900

    Updated 2024.01.12

--- data_structure/test.py
+++ data_structure/test.py
@@ -1,6 +1,4 @@
-## Linked List 구현.
-
-class Node():
+class Node:
     """
     연결 리스트에 의해 연결될 노드에 대한 클래스를 정의한다.
     노드에 저장할 값은 (데이터, 다음 노드 주소, 이전 노드 주소)이다.
@@ -12,77 +10,69 @@ class Node():
         self.prev = None
 
 
-class LinkedList():
+class LinkedList:
     def __init__(self):
         """
         노드들을 활용한 연결리스트 클래스를 정의한다.
         head와 tail은 데이터 값을 저장하지 않는 더미 노드로, 노드 삽입이나 삭제 단계에서 예외 처리를 적용하지 않기 위해 사용한다.
         """
-        self.head = None
-        self.tail = None
         self.num_nodes = 0
 
+        self.head, self.tail = Node(None), Node(None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def search(self, n):
         """
-        n번째 노드를 찾고, 그 노드가 가진 데이터 값을 반환한다.
+        연결 리스트에서 n번째 노드를 찾는 메서드.
+        1부터 시작하는 인덱스를 사용하며, 존재하지 않는 인덱스에 대해서는 None을 반환한다.
         """
-        i = 0
-        curr_node = self.head.next
-        while True:
-            curr_node = curr_node.next
+        current = self.head.next
+        index = 1
+        while current != self.tail:
+            if index == n:
+                return current
+            current = current.next
+            index += 1
+        return None
 
-            if i == n:
-                return curr_node
-            
-
-    def traverse(self):
+    def insert(self, n, data):
         """
-        연결리스트에 있는 모든 노드의 data를 출력한다.
+        연결 리스트의 n번째 위치에 새로운 노드를 삽입하는 메서드.
+        1부터 시작하는 인덱스를 사용하며, 존재하지 않는 인덱스에 대해서는 삽입을 수행하지 않는다.
         """
-        if self.num_nodes > 0:
-            curr_node = self.head.next # 첫번째 노드(시작 노드)
+        new_node = Node(data)
 
-            for _ in range(0, self.num_nodes):
-                print(curr_node.data)
-                curr_node = curr_node.next
-        else:
-            print("연결 리스트에 노드가 존재하지 않습니다.\n")
-        
-
-    def insert(self, n, value):
-        """
-        n번째 노드의 다음에 새로운 노드를 삽입한다.
-        1. 신규 노드의 next는 n번째 노드가 가리키던 n+1번째를 가리키고 n+1번째의 prev는 신규 노드를 가리킨다.
-        2. n번째 노드의 next는 신규 노드를 가리키고, 신규 노드의 prev는 n번째 노드를 가리킨다.
-        """
-        new_node = Node(value)
-
-        if self.num_nodes == 0:
-            self.head.next = new_node
+        if n == 1:  # 리스트의 처음에 삽입하는 경우
+            new_node.next = self.head.next
             new_node.prev = self.head
+            self.head.next.prev = new_node
+            self.head.next = new_node
+        else:
+            prev_node = self.search(n - 1)
+            if prev_node is None or prev_node == self.tail:
+                return  # 존재하지 않는 위치에는 삽입하지 않음
 
-            self.tail.prev = new_node
-            new_node.next = self.tail
-
-        elif 0 < n < self.num_nodes:
-            curr_node = self.search(n)
-
-            new_node.next = curr_node.next      # 신규 노드의 next가 n+1번째 노드를 가리킨다.
-            curr_node.next = new_node           # n번째 노드의 next가 신규 노드를 가리킨다.
-            new_node.prev = curr_node           # 신규 노드의 prev가 n번째 노드를 가리킨다.
-            curr_node.next.prev = new_node      # n+1번째 노드의 prev가 신규 노드를 가리킨다.
+            new_node.next = prev_node.next
+            new_node.prev = prev_node
+            prev_node.next.prev = new_node
+            prev_node.next = new_node
 
         self.num_nodes += 1
 
+    def traverse(self):
+        """
+        연결 리스트의 모든 노드를 순회하며 데이터를 출력하는 메서드.
+        """
+        current = self.head.next
+        while current != self.tail:
+            print(current.data)
+            current = current.next
 
-if __name__ == "__main__":
-    linked_list = LinkedList()
-    linked_list.traverse()
-
-    linked_list.insert(1, 10)
-    linked_list.traverse()
-
-    linked_list.insert(2, 20)
-    linked_list.insert(3, 30)
-    linked_list.traverse()
+# 예시: LinkedList 클래스의 메서드를 사용하여 몇 가지 작업을 수행
+ll = LinkedList()
+ll.insert(1, 10)
+ll.insert(2, 20)
+ll.insert(2, 15)
+ll.insert(3, 30)
+ll.traverse()
