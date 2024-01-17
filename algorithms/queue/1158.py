e1b9e8e61a55c07fe23798f7ab6a91fa086d1656
pervin0527 <zfbtldnz@gmail.com>
2024-01-17 23:59:10 +0900

    Updated 2024.01.17

--- algorithms/queue/1158.py
+++ algorithms/queue/1158.py
@@ -1,26 +1,38 @@
+"""
+https://www.acmicpc.net/problem/1158
+
+10 8 -> <8, 6, 5, 7, 10, 3, 2, 9, 4, 1>
+"""
 import sys
 from collections import deque
 
 N, K = map(int, sys.stdin.readline().rstrip().split())
+
+## 큐 사용
+q = deque()
 result = []
 
-q = deque()
-for num in range(1, N + 1):
-    q.append(num)
+for i in range(1, N+1): 
+    q.append(i)
 
-pos = 0
-while len(q) == 1:
-    for i in range(1, K+1):
-        if pos + i > len(q):
-            pos = 0
-        else:
-            pos += i
+while q:
+    for _ in range(K-1):
+        q.append(q.popleft())
+    result.append(q.popleft())
 
+print(str(result).replace('[', '<').replace(']', '>'))
 
-# for idx, res in enumerate(result):
-#     if idx == 0:
-#         print(f"<{res},", end=" ")
-#     elif idx == len(result) - 1:
-#         print(f"{res}>", end=" ")
-#     else:
-#         print(f"{res},", end=" ")
+## 리스트 사용
+# arr = [num for num in range(1, N+1)]
+# idx = 0
+# result = []
+
+# for number in range(N):
+#     idx += K - 1
+#     if idx >= len(arr):
+#         idx = idx % len(arr)
+    
+#     result.append(str(arr.pop(idx)))
+
+# out = ", ".join(result)
+# print(f"<{out}>")
