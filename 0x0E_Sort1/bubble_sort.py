8fbdaafc4abcb82b9f29e82102bf8731d98bfed7
pervin0527 <zfbtldnz@gmail.com>
2024-06-03 23:28:58 +0900

    Updated 2024.06.03

--- 0x0E_Sort1/bubble_sort.py
+++ 0x0E_Sort1/bubble_sort.py
@@ -1,12 +1,12 @@
-def sort_func(x):
-    n = len(x)
-    for i in range(n): ## 각 반복마다 정렬해야 할 요소의 수가 하나씩 줄어든다.
-        for j in range(n-i-1): ## n-i-1은 마지막 요소가 이미 정렬되었기 때문에, 불필요한 비교를 피하기 위함.
-            if x[j] > x[j+1]:
-                x[j], x[j+1] = x[j+1], x[j]
+def bubble_sort(a):
+    for i in range(len(a)):
+        for j in range(len(a)-i-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
 
-    return x
+    return a
 
 if __name__ == "__main__":
     arr = [9, 1, 6, 8, 4, 3, 2, 0]
-    print(sort_func(arr))
+    result = bubble_sort(arr)
+    print(result)
--- 0x0E_Sort1/selection_sort.py
+++ 0x0E_Sort1/selection_sort.py
@@ -1,14 +1,15 @@
-def sort_func(x):
-    n = len(x)
-    for i in range(n):
-        tmp = i ## 현재 단계에서 맨 앞 자리.
-        for j in range(i+1, n):
-            if x[tmp] > x[j]:
-                tmp = j
-        x[i], x[tmp] = x[tmp], x[i]
-
-    return x
+def selection_sort(a):
+    for i in range(len(a)-1):
+        min_v_idx = i
+        for j in range(i+1, len(a)):
+            if a[min_v_idx] > a[j]:
+                min_v_idx = j    
+        a[i], a[min_v_idx] = a[min_v_idx], a[i]
+        
+    return a
+            
 
 if __name__ == "__main__":
     arr = [9, 1, 6, 8, 4, 3, 2, 0]
-    print(sort_func(arr))
+    result = selection_sort(arr)
+    print(result)
