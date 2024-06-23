b5ffc67a765d8038c81b6caf6284ae37fe9c0cb8
pervin0527 <zfbtldnz@gmail.com>
2024-06-23 18:01:40 +0900

    Updated 2024.06.23

--- 0x10_Dynamic_Programming/9095.py
+++ 0x10_Dynamic_Programming/9095.py
@@ -1,12 +1,17 @@
-if __name__ == "__main__":
-    t = int(input())
+import sys
+input = sys.stdin.readline
+
+def main():
     table = [0] * 11
-    
     table[1], table[2], table[3] = 1, 2, 4
+
     for i in range(4, 11):
         table[i] = table[i-1] + table[i-2] + table[i-3]
 
-    while t > 0:
+    t = int(input())
+    for _ in range(t):
         n = int(input())
         print(table[n])
-        t -= 1
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,43 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def solution1():
-    n = int(input())
-    if n <= 3:
-        print(1)
+def main():
+    table = [0] * 11
+    table[1], table[2], table[3] = 1, 2, 4
 
-    else:
-        table = [0] * 1000000
-        table[0] = n
+    for i in range(4, 11):
+        table[i] = table[i-1] + table[i-2] + table[i-3]
 
-        for i in range(1, len(table)):
-            if table[i-1] % 3 == 0:
-                table[i] = table[i-1] // 3
-            elif table[i-1] % 2 == 0:
-                table[i] = table[i-1] // 2
-            else:
-                table[i] = table[i-1] - 1
-
-            if table[i] == 1:
-                print(i-1)
-                break
-
-
-def solution2():
-    n = int(input())
-    table = [0] * (n+1)
-
-    for i in range(2, n+1):
-        table[i] = table[i-1] + 1
-
-        if i % 2 == 0:
-            table[i] = min(table[i], table[i//2] + 1)
-
-        if i % 3 == 0:
-            table[i] = min(table[i], table[i//3] + 1)
-
-    print(table[n])
-
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print(table[n])
 
 if __name__ == "__main__":
-    solution2()
+    main()
