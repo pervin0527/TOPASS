b3b85c6c5b258c20a966fb8f1ad769c18bcaa6bd
pervin0527 <zfbtldnz@gmail.com>
2024-08-22 09:31:31 +0900

    Updated 2024.08.22

--- 0x03_Array/1475.py
+++ 0x03_Array/1475.py
@@ -1,28 +1,42 @@
-"""
-https://www.acmicpc.net/problem/1475
+import sys
+import math
+input = sys.stdin.readline
 
-한 세트가 0부터 9까지 숫자가 하나씩 들어 있을 때 입력된 방번호를 만들 수 있는 세트의 수.
-6은 9로, 9는 6으로 뒤집어서 사용이 가능하다.
-"""
+# def solution():
+#     """
+#     문제점 : 입력이 0으로만 구성되었을 때를 처리할 수 없다. 즉, 0을 카운팅할 수 없다.
+#     """
+#     room_num = int(input())
+    
+#     p_nums = [0] * 10
+#     while room_num > 0:
+#         remain = room_num % 10
+#         quote = room_num // 10
 
-room = input()
-numbers = [0] * 10
+#         if remain == 6 and p_nums[remain] > 1:
+#             p_nums[9] += 1
+#         elif remain == 9 and p_nums[remain] > 1:
+#             p_nums[6] += 1
+#         else:
+#             p_nums[remain] += 1
 
-for r in room:
-    r = int(r)
+#         room_num = quote
 
-    if r == 6 and numbers[r] > 0:
-        if numbers[9] == numbers[r] - 1:
-            numbers[9] += 1
-        else:
-            numbers[r] += 1
+#     print(max(p_nums))
 
-    elif r == 9 and numbers[r] > 0:
-        if numbers[6] == numbers[r] - 1:
-            numbers[6] += 1
-        else:
-            numbers[r] += 1
-    else:
-        numbers[r] += 1
 
-print(max(numbers))
+def solution():
+    room_num = input().strip()
+    
+    p_nums = [0] * 10
+    for digit in room_num:
+        p_nums[int(digit)] += 1
+    
+    # 6과 9를 합쳐서 필요한 세트를 계산
+    six_nine_count = p_nums[6] + p_nums[9]
+    p_nums[6] = p_nums[9] = math.ceil(six_nine_count / 2) ## math.ceil : 2.xx 인 경우 3으로 올림.
+    
+    print(max(p_nums))
+
+if __name__ == "__main__":
+    solution()
---
+++ error_logs.md
@@ -0,0 +1,4 @@
+# Array
+
+- [BOJ 2577](https://www.acmicpc.net/problem/2577)
+- [BOJ 1475](https://www.acmicpc.net/problem/1475)
